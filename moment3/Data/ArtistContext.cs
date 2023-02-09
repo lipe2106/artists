7977719a7064c117aa6f2e3037f06952b32e0b9f
Lina Petersson <lipe2106@student.miun.se>
2023-02-09 09:31:14 +0100

    Skapat grund, installerat EF och gjort en migration

---
+++ moment3/Data/ArtistContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using moment3.Models; // Include model
+
+namespace moment3.Data
+{
+    public class ArtistContext : DbContext // Inherit from class DbContext
+    {
+        // Contructor with settings. Save ArtistContext in variable options and send to base class
+        public ArtistContext(DbContextOptions<ArtistContext> options) : base(options) 
+        { 
+        }
+
+        // Link Context to Artist model and add name to database table
+        public DbSet<Artist> Artist { get; set; }
+    }
+}
---
+++ moment3/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace moment3.Models
+{
+    public class Artist
+    {
+        // Properties
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
